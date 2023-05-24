d2afa7079c1f802a6355f152e07f434fc1185f1a
Luke Curley <kixelated@gmail.com>
2023-05-24 16:36:24 -0700

    Fetch the fingerprint via HTTPS.
    It's less efficient but it allows warp-rs and warp-js to be separate
    repositories.

--- src/index.ts
+++ src/index.ts
@@ -3,19 +3,15 @@ import Transport from "./transport"
 
 const params = new URLSearchParams(window.location.search)
 
-const url = params.get("url") || "https://localhost:4443/watch"
-const canvas = document.querySelector<HTMLCanvasElement>("canvas#video")!
+const url = params.get("url") || "https://localhost:4443"
+const fingerprintUrl = url + "/fingerprint"
 
 const transport = new Transport({
-	url: url,
-	/*
-	fingerprint: {
-		// TODO remove when Chrome accepts the system CA
-		algorithm: "sha-256",
-		value: new Uint8Array(fingerprint),
-	},
-	*/
+	url,
+	fingerprintUrl,
 })
+
+const canvas = document.querySelector<HTMLCanvasElement>("canvas#video")!
 
 const player = new Player({
 	transport,
