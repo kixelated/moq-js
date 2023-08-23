4828efe616e110f1925e3821c9a292a712151fb4
Luke Curley <kixelated@gmail.com>
2023-08-23 10:44:07 -0700

    Parcel doesn't work within a workspace unfortunately.
    This is fine TM.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ["./src/**/*.{html,js,ts,jsx,tsx}"],
+	content: ["./web/**/*.{html,js,ts,jsx,tsx}"],
 	theme: {
 		extend: {
 			transitionProperty: {
