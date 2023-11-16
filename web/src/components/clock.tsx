60553082fbdd5b577e0e99228a4338ddad0dae4f
Luke Curley <kixelated@gmail.com>
2023-11-16 15:21:08 +0900

    Add a clock for BBB only.

---
+++ web/src/components/clock.tsx
@@ -0,0 +1,25 @@
+import { onMount, onCleanup } from "solid-js"
+
+// Print the current time to check the latency.
+export default function Clock() {
+	let element!: HTMLDivElement
+
+	onMount(() => {
+		let frame: number
+
+		const repaint = () => {
+			element.textContent = "browser: " + new Date().toISOString().split("T")[1].slice(0, 12)
+			frame = requestAnimationFrame(repaint)
+		}
+
+		frame = requestAnimationFrame(repaint)
+		onCleanup(() => cancelAnimationFrame(frame))
+	})
+
+	return (
+		<div class="mx-6 my-3 flex flex-row font-mono">
+			<div class="text-yellow-300">note: clocks are not synchronized</div>
+			<div class="flex-grow text-right" ref={element} />
+		</div>
+	)
+}
